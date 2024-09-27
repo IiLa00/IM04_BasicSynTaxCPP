0d38e3c8830e5b6f19e0a8f21b7c095beb85176e
김현준 <154495800+IiLa00@users.noreply.github.com>
2024-09-27 19:52:00 +0900

    0927_Property

--- Source/BasicSynTaxCPP/01_Property/CProperty.cpp
+++ Source/BasicSynTaxCPP/01_Property/CProperty.cpp
@@ -6,14 +6,24 @@ ACProperty::ACProperty()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	NumB = 999;
+
+	// When this construct is called
+	GLog->Log("ACProperty::ACProperty()");
+
 }
 
 void ACProperty::BeginPlay()
 {
 	Super::BeginPlay();
 
-	GLog->Log("Hello Wolrd!");
-	
+	FString Message;
+	//Message.Append("NumA = ");
+	//Message.Append(FString::FromInt(NumA));
+
+	Message = FString::Printf(TEXT("NumA = %d, NumB = %d, NumC = %d"), NumA, NumB, NumC);
+
+	GEngine->AddOnScreenDebugMessage(-1,5.f,FColor::White,Message);
 }
 
 void ACProperty::Tick(float DeltaTime)
